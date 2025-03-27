f3e3db4e184b007b52283757ffefd2575b0867dc
Rohit Jain <100214764+rohitjainfsl@users.noreply.github.com>
2025-03-27 18:34:32 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,7 +33,7 @@ export async function loginUser(req, res) {
     if (!bcrypt.compare(password, checkUser.password)) {
       return res.status(400).json({ error: "Invalid Password" });
     }
-    if(!checkUser.role !== role)  return res.status(400).json({ error: "Invalid Role" });
+    if(checkUser.role !== role)  return res.status(400).json({ error: "Invalid Role" });
 
     //Create a token using JWT
     const token = generateToken(checkUser);
